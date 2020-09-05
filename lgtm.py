0cefaddd1b0333a6e1acaa0f8c31aed26e10b877
Jonathan Leitschuh <Jonathan.Leitschuh@gmail.com>
2020-09-05 17:14:08 -0400

    Add a way to get all subscribed projects

--- lgtm.py
+++ lgtm.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from typing import Optional, List
+from typing import Optional, List, Dict
 
 import requests
 import yaml
@@ -21,8 +21,39 @@ class LGTMSite:
     def _headers(self):
         return {
             'LGTM-Nonce': self.nonce
-
         }
+
+    def _make_lgtm_get(self, url: str) -> dict:
+        r = requests.get(
+            url,
+            cookies=self._cookies(),
+            headers=self._headers()
+        )
+        return r.json()
+
+    def get_my_projects(self) -> List[dict]:
+        url = 'https://lgtm.com/internal_api/v0.2/getMyProjects?apiVersion=' + self.api_version
+        data = self._make_lgtm_get(url)
+        if data['status'] == 'success':
+            return data['data']
+        else:
+            raise Exception('LGTM GET request failed with response: %s' % str(data))
+
+    def _make_lgtm_post(self, url: str, data: dict) -> dict:
+        api_data = {
+            'apiVersion': self.api_version
+        }
+        full_data = {**api_data, **data}
+        print(data)
+        r = requests.post(
+            url,
+            full_data,
+            cookies=self._cookies(),
+            headers=self._headers()
+        )
+        data_returned = r.json()
+        print(data_returned)
+        return data_returned
 
     def load_into_project_list(self, into_project: int, lgtm_project_ids: List[str]):
         url = "https://lgtm.com/internal_api/v0.2/updateProjectSelection"
@@ -32,30 +63,32 @@ class LGTMSite:
             'projectSelectionId': into_project,
             'addedProjects': '[' + list_serialized + ']',
             'removedProjects': '[]',
-            'apiVersion': self.api_version
         }
-        print(data)
-        r = requests.post(url, data, cookies=self._cookies(), headers=self._headers())
-        data_returned = r.json()
-        print(data_returned)
+        self._make_lgtm_post(url, data)
 
-    def load_repository(self, repository_url: str):
+    def follow_repository(self, repository_url: str):
         url = "https://lgtm.com/internal_api/v0.2/followProject"
         data = {
             'url': repository_url,
             'apiVersion': self.api_version
         }
-        r = requests.post(url, data, cookies=self._cookies(), headers=self._headers())
-        data_returned = r.json()
-        print(data_returned)
+        self._make_lgtm_post(url, data)
+
+    def unfollow_repository_by_id(self, project_id: int):
+        url = "https://lgtm.com/internal_api/v0.2/unfollowProject"
+        data = {
+            'project_key': project_id,
+            'apiVersion': self.api_version
+        }
+        self._make_lgtm_post(url, data)
 
     @staticmethod
-    def retrieve_project_id(gh_project_path: str) -> Optional[str]:
+    def retrieve_project_id(gh_project_path: str) -> Optional[int]:
         url = "https://lgtm.com/api/v1.0/projects/g/" + gh_project_path
         r = requests.get(url)
         data_returned = r.json()
         if 'id' in data_returned:
-            return str(data_returned["id"])
+            return int(data_returned["id"])
         else:
             return None
 
@@ -63,10 +96,44 @@ class LGTMSite:
     def create_from_file() -> 'LGTMSite':
         with open("config.yml") as config_file:
             config = yaml.safe_load(config_file)
-            lgtm: dict = config.lgtm
+            lgtm: dict = config['lgtm']
             return LGTMSite(
                 nonce=lgtm['nonce'],
                 long_session=lgtm['long_session'],
                 short_session=lgtm['short_session'],
                 api_version=lgtm['api_version'],
             )
+
+
+class LGTMDataFilters:
+
+    @staticmethod
+    def org_to_ids(projects: List[Dict]) -> Dict[str, List[Dict]]:
+        """
+        Converts the output from :func:`~lgtm.LGTMSite.get_my_projects` into a dic of GH org
+        to list of projects including their GH id and LGTM id.
+        """
+        org_to_ids = {}
+        for project in projects:
+            if 'protoproject' in project:
+                print('skipping %s' % project['protoproject']['displayName'])
+                continue
+            if 'realProject' not in project:
+                raise KeyError('\'realProject\' not in %s' % str(project))
+            the_project = project['realProject'][0]
+            if the_project['repoProvider'] != 'github_apps':
+                # Not really concerned with BitBucket right now
+                continue
+            org = str(the_project['slug']).split('/')[1]
+            ids_list: List[Dict]
+            if org in org_to_ids:
+                ids_list = org_to_ids[org]
+            else:
+                ids_list = []
+                org_to_ids[org] = ids_list
+            ids_list.append({
+                'display_name': the_project['displayName'],
+                'key': the_project['key']
+            })
+
+        return org_to_ids
