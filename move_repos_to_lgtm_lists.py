8e839a8fa9d515a23c1f7807988a8dcd1264144a
Thank You <admin@miyamotocorp.com>
2021-02-17 16:21:00 -0500

    Refactor code

--- move_repos_to_lgtm_lists.py
+++ move_repos_to_lgtm_lists.py
@@ -5,19 +5,14 @@ import sys
 import time
 import os
 
-cached_files = os.listdir("cache")
-site = LGTMSite.create_from_file()
-
-for cached_file_name in cached_files:
-    # This is dirty. Is there an easier way to do this?
-    cached_file = "cache/" + cached_file_name
-
+def get_project_list_id(cached_file_name: str) -> str:
     project_list_name = cached_file_name.split(".")[0]
 
     # We want to find or create a project list based on the the name of
     # the text file that holds all of the projects we are currently following.
-    project_list_id = site.get_or_create_project_list(project_list_name)
+    return site.get_or_create_project_list(project_list_name)
 
+def get_project_ids(cached_file: str) -> List[str]:
     file = open(cached_file, "r")
 
     project_ids = file.read().split("\n")
@@ -25,15 +20,33 @@ for cached_file_name in cached_files:
     # remove any "" in the array
     while("" in project_ids):
         project_ids.remove("")
-    # With the project list id and the project ids, we now want to save the repos
-    # we currently follow to the project list
 
-    site.load_into_project_list(project_list_id, project_ids)
+    return project_ids
 
+def cleanup(file_name: str):
+    # Since we are done with the file, we can now delete it from the cache.
+    os.remove(file_name)
+
+def unfollow_projects(project_ids: List[str]):
     for project_id in project_ids:
         # The last thing we need to do is tidy up and unfollow all the repositories
         # we just added to our project list.
         site.unfollow_repository_by_id(project_id)
 
-    # Since we are done with the file, we can now delete it from the cache.
-    os.remove(cached_file)
+def process_cached_file(cached_file_name):
+    cached_file = "cache/" + cached_file_name
+    project_list_id = get_project_list_id(cached_file_name)
+    project_ids = get_project_ids(cached_file)
+
+    # With the project list id and the project ids, we now want to save the repos
+    # we currently follow to the project list
+    site.load_into_project_list(project_list_id, project_ids)
+
+    unfollow_projects(project_ids)
+    cleanup(cached_file)
+
+site = LGTMSite.create_from_file()
+cached_file_names = os.listdir("cache")
+
+for cached_file_name in cached_file_names:
+    process_cached_file(cached_file_name)
