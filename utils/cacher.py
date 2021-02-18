b5ecc8a528cfd39d885775db90f22dc6419ff5ac
Thank You <admin@miyamotocorp.com>
2021-02-17 22:31:35 -0500

    Create cache folder if it already doesn't exist

--- utils/cacher.py
+++ utils/cacher.py
@@ -1,7 +1,16 @@
 from typing import List
+import os
+
+def create_cache_folder():
+    if not os.path.exists('cache'):
+        os.makedirs('cache')
 
 def write_project_ids_to_file(project_ids: List[str], file_name: str):
+    create_cache_folder()
+
     file = open("cache/" + file_name + ".txt", "a")
+
     for project_id in project_ids:
         file.write(project_id + "\n")
+
     file.close()
